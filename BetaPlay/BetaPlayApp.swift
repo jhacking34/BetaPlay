167d717d0af95ca6aebdea47906572531711ad46
Johnny Hacking <jhacking@bhico.com>
2021-06-08 23:39:29 -0600

    Initial Commit

---
+++ BetaPlay/BetaPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetaPlayApp.swift
+//  BetaPlay
+//
+//  Created by Johnny Hacking on 6/8/21.
+//
+
+import SwiftUI
+
+@main
+struct BetaPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
