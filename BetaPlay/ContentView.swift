6601e2c22add55ffb0a925bbcded5bdec87de6d6
Johnny Hacking <jhacking@bhico.com>
2021-06-10 07:44:46 -0600

    Just playing around

--- BetaPlay/ContentView.swift
+++ BetaPlay/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var people = [
+    Profile(name: "Johnny", avatar: "person"),
+    Profile(name: "Shaylee", avatar: "person.crop.circle"),
+    Profile(name: "Macady", avatar: "person.badge.plus")]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("My People")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+            HStack{
+                ForEach(people){ person in
+                    Image(systemName: person.avatar)
+                    Text(person.name)
+                }
+                .font(.title2)
+            }
+            List{
+                ForEach($people){ $person in
+                    if #available(iOS 15.0, *) {
+                        Label{
+                            TextField("Name", text: $person.name)
+                        } icon: {
+                            Image(systemName: person.avatar)
+                        }
+                        .listRowSeparator(.hidden)
+                    } else {
+                        // Fallback on earlier versions
+                    }
+                }
+            }
+            
+            
+            Spacer()
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12")
     }
 }
+
+struct Profile: Identifiable{
+    var id = UUID()
+    var name: String
+    var avatar: String
+}
+
